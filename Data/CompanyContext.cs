6afe2c76848eeb2b4f4b6ece5709b7a8d0cf10f8
jharck <javier.rc@gmail.com>
2020-09-16 01:41:22 -0600

    Initial commit for OneLinkCompany project

---
+++ Data/CompanyContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace OneLinkCompany.Data
+{
+    public class CompanyContext : DbContext
+    {
+        
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OneLinkCompany.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
